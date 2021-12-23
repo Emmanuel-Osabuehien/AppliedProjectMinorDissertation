dd8bcfb03bc01639868734383848b8db4bebdb64
Emmanuel-Osabuehien <g00373559@gmit.ie>
2021-12-23 20:35:54 +0000

    Adding Charts To App and Displaying Charts In Table

--- meal-planning-system/src/pages/Requirements/index.js
+++ meal-planning-system/src/pages/Requirements/index.js
@@ -1,85 +1,158 @@
 import './requirements.css';
 import 'chart.js/auto';
-import {Bar, Pie, Doughnut} from 'react-chartjs-2';
-
+import { Pie } from 'react-chartjs-2';
+import { Table } from "react-bootstrap";
 
 function RequirementsPage() {
   return (
-    <div className="Graphs">
-      <h5 className="mb-4">Balanced Diet</h5>
+    
+      <Table className="Graphs" bordered hover size="sm">
+      <tbody>
+        <tr>
+        <td>
+        <h5 className="mt-2">Balanced Diet</h5>
       <Pie
-      data={{
-        labels:['Fruits & Veg','Proteins','Carbohydrates','Fats'],
-        datasets:[{
-          data:[40,25,25,10],
-          backgroundColor:['red','green','orange','blue'],
-        }]
-      }
-      }
+        data={{
+          labels: ['Fruits & Veg', 'Proteins', 'Carbohydrates', 'Fats'],
+          datasets: [{
+            data: [40, 25, 25, 10],
+            backgroundColor: ['red', 'green', 'orange', 'blue'],
+          }]
+        }
+        }
       >
 
       </Pie>
-<hr></hr>
-      <h5 className="mt-4 mb-4">Vegan Diet</h5>
-      <Pie
-      data={{
-        labels:['Fruits & Veg','Carbohydrates','Fats'],
-        datasets:[{
-          data:[40, 55, 15],
-          backgroundColor:['red','green','orange'],
-        }]
-      }
-      }
+      </td>
+      <td>
+      <h5 className="mt-2">Vegan Diet</h5>
+     <Pie
+        data={{
+          labels: ['Fruits & Veg', 'Carbohydrates', 'Fats'],
+          datasets: [{
+            data: [40, 55, 15],
+            backgroundColor: ['red', 'green', 'orange'],
+          }]
+        }
+        }
       >
 
       </Pie>
-
-      <hr></hr>
-      <h5 className="mt-4 mb-4">Paleo Diet</h5>
+      </td>
+      <td>
+      <h5 className="mt-2">Paleo Diet</h5>
       <Pie
-      data={{
-        labels:['Fruits & Veg','Proteins','Carbohydrates','Fats'],
-        datasets:[{
-          data:[40,30,15,15],
-          backgroundColor:['red','green','orange','blue'],
-        }]
-      }
-      }
+        data={{
+          labels: ['Fruits & Veg', 'Proteins', 'Carbohydrates', 'Fats'],
+          datasets: [{
+            data: [40, 30, 15, 15],
+            backgroundColor: ['red', 'green', 'orange', 'blue'],
+          }]
+        }
+        }
       >
 
       </Pie>
-
-      <hr></hr>
-      <h5 className="mt-4 mb-4">Mediterranean Diet</h5>
+      </td>
+      </tr>
+      <tr>
+      <td>
+      <h5 className="mt-2">Mediterranean  Diet</h5>
       <Pie
-      data={{
-        labels:['Fruits & Veg','Proteins','Carbohydrates','Fats'],
-        datasets:[{
-          data:[30,20,35,15],
-          backgroundColor:['red','green','orange','blue'],
-        }]
-      }
-      }
+        data={{
+          labels: ['Fruits & Veg', 'Proteins', 'Carbohydrates', 'Fats'],
+          datasets: [{
+            data: [30, 20, 35, 15],
+            backgroundColor: ['red', 'green', 'orange', 'blue'],
+          }]
+        }
+        }
       >
 
       </Pie>
-
-      <hr></hr>
-      <h5 className="mt-4 mb-4">Low Carbohydrates Diet</h5>
+      </td>
+      <td>
+      <h5 className="mt-2">Low-Carb Diet</h5>
       <Pie
-      data={{
-        labels:['Proteins','Carbohydrates','Fats'],
-        datasets:[{
-          data:[50, 30, 20],
-          backgroundColor:['red','green','orange'],
-        }]
-      }
-      }
+        data={{
+          labels: ['Proteins', 'Carbohydrates', 'Fats'],
+          datasets: [{
+            data: [50, 30, 20],
+            backgroundColor: ['red', 'green', 'orange'],
+          }]
+        }
+        }
       >
 
       </Pie>
+      </td>
+      <td>
+      <h5 className="mt-2">Ketogenic Diet</h5>
+      <Pie
+        data={{
+          labels: ['Proteins', 'Carbohydrates', 'Fats'],
+          datasets: [{
+            data: [20, 5, 75],
+            backgroundColor: ['red', 'green', 'orange'],
+          }]
+        }
+        }
+      >
+      </Pie>
+      </td>
+      </tr>
+<tr>
+<td>
+      <h5 className="mt-2">The Zone Diet</h5>
+      <Pie
+        data={{
+          labels: ['Proteins', 'Carbohydrates', 'Fats'],
+          datasets: [{
+            data: [30, 40, 20],
+            backgroundColor: ['red', 'green', 'orange'],
+          }]
+        }
+        }
+      >
 
-    </div>
+      </Pie>
+      </td>
+      <td>
+      <h5 className="mt-2">Atkins Diet</h5>
+      <Pie
+        data={{
+          labels: ['Proteins', 'Carbohydrates', 'Fats'],
+          datasets: [{
+            data: [30, 65, 5],
+            backgroundColor: ['red', 'green', 'orange'],
+          }]
+        }
+        }
+      >
+
+      </Pie>
+      </td>
+
+      <td>
+      <h5 className="mt-2">Ultra-Low-Fat Diet</h5>
+      <Pie
+        data={{
+          labels: ['Proteins', 'Carbohydrates', 'Fats'],
+          datasets: [{
+            data: [10, 80, 10],
+            backgroundColor: ['red', 'green', 'orange'],
+          }]
+        }
+        }
+      >
+
+      </Pie>
+      </td>
+</tr>
+      </tbody>
+      </Table>
+
+ 
   );
 }
 
